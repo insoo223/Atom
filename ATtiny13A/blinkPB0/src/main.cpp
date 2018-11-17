67aac963ff11cc8bd752ba126d5659ed572ac8f0
insoo223 <insoo@hotmail.com>
2018-11-17 13:14:36 +0900

    Add Atom IDE git repository from gitDesktop
    Atom with PlatformIO

---
+++ ATtiny13A/blinkPB0/src/main.cpp
@@ -0,0 +1,19 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    // make the LED pin an output for PORTB5
+    //DDRB = 1 << 1;
+    DDRB |= _BV(1);
+
+    while (1)
+    {
+        _delay_ms(1000);
+
+        // toggle the LED
+        PORTB ^= _BV(1);
+    }
+
+    return 0;
+}
